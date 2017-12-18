9b6eb3a7f12aa10e209cf07f9781ffe4f5914678
Keisan <keisangittens@gmail.com>
2017-12-18 20:03:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+=======
+var greetSpecialGuest = "Hello" + specialGuest + "!";
+>>>>>>> c4a2d280e34035d489094ef9bcc443a62504ca2e
 
-var topic = "space";
+var topic = "Let\'s talk about space.";
 
 var conversation = "Let\'s talk about "+ `${topic}.`;
